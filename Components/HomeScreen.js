54604b2e39a71b2b678c9ef74c8dd60f5cad7197
Vivek Singh <vs2357766@gmail.com>
2021-07-22 14:11:37 +0530

    restored

---
+++ Components/HomeScreen.js
@@ -0,0 +1,65 @@
+import 'react-native-gesture-handler';
+import React ,{Component}from 'react';
+import { useState } from 'react';
+
+
+import { View,Text,Button,StyleSheet,TextInput } from 'react-native';
+import { black } from 'chalk';
+
+
+
+const HomeScreen = ({ navigation }) => {
+  var Showtext="asdd";
+ 
+  const [textInputValue, setTextInputValue] = React.useState('');
+    return (
+      
+      <View>
+        <Text testID='welcome'>Get Your Movie</Text>
+        <View style={{marginTop:40}} > 
+        <Button
+        title="List"
+        onPress={() =>
+          navigation.navigate('List') }
+        />
+        </View>
+
+        <View style={{marginTop: 20}}>
+        < Button 
+        title="Search" 
+        onPress={() =>
+          navigation.navigate('Search') }
+        />
+        </View>
+
+        <View style={{margin:20}}>
+          <Text >{textInputValue}</Text>
+        </View>
+
+        <View>
+        <TextInput 
+          style= {styles.textInputStyle}
+          value={textInputValue}
+          underlineColorAndroid="transparent"
+          onChangeText={text => setTextInputValue(text)}
+	        placeholder="Insert your text!"
+          />
+        </View>
+      </View>
+    );
+  };
+
+  const styles =StyleSheet.create({
+    textInputStyle:{
+      height: 50,
+      borderWidth: 1,
+      paddingLeft: 20,
+      margin: 5,
+      borderColor: '#009688',
+      backgroundColor: 'white'
+    }
+  });
+
+  export  default HomeScreen;
+
+
